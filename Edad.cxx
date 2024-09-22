63acb7cae7edbbb2938de9ee2c8f01d2a938f774
Alex Gamez <alexgamr27@gmail.com>
2024-09-22 13:02:17 -0700

    Add files via upload

---
+++ Edad.cxx
@@ -0,0 +1,19 @@
+#include <cstdio>
+
+int main()
+{
+     char nombre [20];
+     int Anac;
+     int Aact=2024;
+     
+     printf ("introduce tu nombre");
+     scanf ("%s", nombre);
+     printf ("introduce tu año de nacimiento");
+     scanf ("%d", &Anac);
+     
+     int edad= (Aact - Anac);
+     
+     printf ("Hola %s, tu edad es %d", nombre, edad);
+     
+     return 0;
+}
