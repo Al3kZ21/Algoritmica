99c7ce9b99898f834f2f48d57eb454d68ca551a5
Alex Gamez <alexgamr27@gmail.com>
2024-09-22 13:30:13 -0700

    Add files via upload

---
+++ Nombre iostream.cxx
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+     char Name [20], Lname [20];
+     
+     cout <<"Ingresa tu nombre: ";
+     cin >> Name;
+     
+     cout << "Ingresa tu apellido: ";
+     cin >> Lname;
+     
+     cout <<"Hola, "<< Name << " " <<Lname;
+     
+     return 0;
+}
