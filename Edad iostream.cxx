ed796a030efdc96e5c154998887be65fa2d4f8d3
Alex Gamez <alexgamr27@gmail.com>
2024-09-22 13:30:31 -0700

    Add files via upload

---
+++ Edad iostream.cxx
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+     int Ano, edad;
+     
+     cout <<"Ingresa Ano de Nacimiento: ";
+     cin >> Ano;
+     
+     edad= 2024-Ano;
+     
+     cout <<"tu edad es: "<< edad << "años" <<endl;
+     
+     return 0;
+}
