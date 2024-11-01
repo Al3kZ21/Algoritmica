940b2e1270c75a90f2d4e197c09d4bde4f9885b9
Alexander Gamez <alexgamr27@gmail.com>
2024-11-01 10:37:47 -0700

    Delete hola mundo 2.cxx

--- hola mundo 2.cxx
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main ()
-{
-    printf ("hola mundo");
-    printf ("\n hola mundo");
-    printf ("\n X  \n Y");
-    printf ("\n\n hola mundo \a");
-    printf ("\n\n\n  \ hola mundo");
-}
